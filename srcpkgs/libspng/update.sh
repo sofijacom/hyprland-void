f49136347979cff5083e07fa982abd3a4453d4f9
sofijacom <sofija.p2018@gmail.com>
2026-06-25 15:03:12 +0300

    Update template file

---
+++ srcpkgs/libspng/update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+printf "Checking latest version\n"
+
+__dir="$(dirname "${BASH_SOURCE[0]}")"
+
+TPL=${__dir}/template
+REPO="randy408/libspng"
+
+LATEST_VERSION=$(gh release list --repo ${REPO} --exclude-drafts --exclude-pre-releases --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+export VERSION=${LATEST_VERSION#"v"}
+CURRENT_VERSION=$(grep -E '^version=' "${TPL}" | cut -d= -f2)
+
+printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
+[ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
+
+# No preprepped checksum files, need to download the binary and calculate it myself
+gh release download -R ${REPO} --archive=tar.gz --output "v${VERSION}.tar.gz"
+export SHA256=$(sha256sum ./v${VERSION}.tar.gz | cut -d ' ' -f1 )
+rm ./v${VERSION}.tar.gz
+[[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
+
+sed -i "s/^version=.*/version=$VERSION/" "$TPL"
+sed -i "s/^checksum=.*/checksum=${SHA256}/" "$TPL"
+
+printf " template updated\n"
